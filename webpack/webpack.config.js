fe9a527b5b16b5ab61289d59782aa8a294551cf6
Nancy <819128401@qq.com>
2016-12-11 15:26:06 +0800

    Update webpack.config.js

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -14,15 +14,7 @@ module.exports = {
         new webpack.optimize.UglifyJsPlugin(),//压缩JS代码
         new ExtractTextPlugin("[name].css")
     ],
-    //页面入口文件配置
-    /*entry: {
-        index : './html5/cdfer/webpack/resource/js/index.js'
-    },
-    //入口文件输出配置
-    output: {
-        path: './html5/cdfer/webpack/assets/js/',
-        filename: '[name].js'
-    },*/
+   
     devtool: 'eval-source-map',//配置生成Source Maps，调试的时候用：'eval-source-map'，正式服用：'source-map';
     module: {
         //加载器配置
@@ -39,18 +31,12 @@ module.exports = {
         require('autoprefixer')
     ],
     devServer: {
-        //contentBase: "./public",//本地服务器所加载的页面所在的目录
         colors: true,//终端中输出结果为彩色
-        //historyApiFallback: true,//不跳转
         inline: true//实时刷新
     } 
     //其它解决方案配置
     resolve: {//解析模块路径时的配置
         root: 'D:/appServ/www/workSpace',
-        extensions: ['', '.js', '.jsx'],
-        alias: {
-            jquery:'/Common/js/jquery-2.1.4.min.js',
-            skocket:'/html5/cdfer/webpack/resource/webscocket.js'
-        }
+        extensions: ['', '.js', '.jsx']
     }
 };
